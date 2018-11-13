e61338e5d4b9946deb89ad870389268d89325af8
jchai <j.chai91@gmail.com>
2018-11-13 14:55:17 -0800

    Adds outfit layout

---
+++ app/src/main/java/com/example/michael/myapplication/CreateOutfit.java
@@ -0,0 +1,125 @@
+package com.example.michael.myapplication;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PointF;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class CreateOutfit extends AppCompatActivity {
+
+    public static final int SELECT_STICKER_REQUEST_CODE = 123;
+    protected MotionView motionView;
+    //Button addButton;
+    protected ImageView imageEntity;
+    private final MotionView.MotionViewCallback motionViewCallback = new MotionView.MotionViewCallback() {
+
+            @Override
+            public void onEntitySelected(@Nullable MotionEntity entity) {
+                if (entity instanceof ImageEntity) {
+                    //imageEntity.setImageResource();
+                }
+            }
+        @Override
+        public void onEntityDoubleTap(@NonNull MotionEntity entity) {
+            imageEntity.setVisibility(View.GONE);
+        }
+
+        };
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_outfit);
+
+        motionView = (MotionView) findViewById(R.id.main_motion_view);
+        motionView.setMotionViewCallback(motionViewCallback);
+
+
+        //addButton = (Button)findViewById(R.id.addClothesButton);
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.create_outfit_toolbar_id);
+        setSupportActionBar(toolbar);
+
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    //public void addClothes(View view){
+        //Intent intent = new Intent(this, CreateOutfitSticker.class);
+        //startActivityForResult(intent, SELECT_STICKER_REQUEST_CODE);
+
+    //}
+
+    public void addtoOutfitButton(View view){
+        Intent intent = new Intent(this, CreateOutfitSticker.class);
+        startActivityForResult(intent, SELECT_STICKER_REQUEST_CODE);
+    }
+
+    private void addSticker(final int stickerResId) {
+        motionView.post(new Runnable() {
+            @Override
+            public void run() {
+                Layer layer = new Layer();
+                Bitmap item = BitmapFactory.decodeResource(getResources(), stickerResId);
+
+                ImageEntity entity = new ImageEntity(layer, item, motionView.getWidth(), motionView.getHeight());
+
+                motionView.addEntityAndPosition(entity);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.create_outfit_menu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_STICKER_REQUEST_CODE) {
+                if (data != null) {
+                    int stickerId = data.getIntExtra(CreateOutfitSticker.EXTRA_STICKER_ID, 0);
+                    if (stickerId != 0) {
+                        addSticker(stickerId);
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.main_add_sticker) {
+            Intent intent = new Intent(this, CreateOutfitSticker.class);
+            startActivityForResult(intent, SELECT_STICKER_REQUEST_CODE);
+            return true;
+        } else if (item.getItemId() == R.id.main_save_outfit) {
+            //save
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/michael/myapplication/CreateOutfitSticker.java
@@ -0,0 +1,119 @@
+package com.example.michael.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateOutfitSticker extends AppCompatActivity {
+
+    public static final String EXTRA_STICKER_ID = "extra_sticker_id";
+    private int[] stickerIds = {
+            R.drawable.shirt1,
+            R.drawable.womens_black_jeans,
+            R.drawable.gold_high_heels,
+            R.drawable.gold_spaghetti_top
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_sticker_recycler_view);
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar_id);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.stickers_recycler_view);
+        GridLayoutManager glm = new GridLayoutManager(this, 3);
+        recyclerView.setLayoutManager(glm);
+
+        List<Integer> stickers = new ArrayList<>(stickerIds.length);
+        for (Integer id : stickerIds) {
+            stickers.add(id);
+        }
+
+        recyclerView.setAdapter(new StickersAdapter(stickers, this));
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onStickerSelected(int stickerId) {
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_STICKER_ID, stickerId);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    class StickersAdapter extends RecyclerView.Adapter<StickersAdapter.StickerViewHolder> {
+
+        private final List<Integer> stickerIds;
+        private final Context context;
+        private final LayoutInflater layoutInflater;
+
+        StickersAdapter(@NonNull List<Integer> stickerIds, @NonNull Context context) {
+            this.stickerIds = stickerIds;
+            this.context = context;
+            this.layoutInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public StickerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new StickerViewHolder(layoutInflater.inflate(R.layout.activity_create_outfit_sticker, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(StickerViewHolder holder, int position) {
+            holder.image.setImageDrawable(ContextCompat.getDrawable(context, getItem(position)));
+        }
+
+        @Override
+        public int getItemCount() {
+            return stickerIds.size();
+        }
+
+        private int getItem(int position) {
+            return stickerIds.get(position);
+        }
+
+        class StickerViewHolder extends RecyclerView.ViewHolder {
+
+            ImageView image;
+
+            StickerViewHolder(View itemView) {
+                super(itemView);
+                image = (ImageView) itemView.findViewById(R.id.sticker_image);
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int pos = getAdapterPosition();
+                        if (pos >= 0) { // might be NO_POSITION
+                            onStickerSelected(getItem(pos));
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
