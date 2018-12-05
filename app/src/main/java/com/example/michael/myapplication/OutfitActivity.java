f33640e583989536b572ae411990b8e5ba5af730
SamierM <samier.mahagna@gmail.com>
2018-12-05 10:35:29 -0800

    Properly Displays Outfits
    Crashes when outfits are clicked on.
    I suspsect this is due to the way outfit cards are created.

--- app/src/main/java/com/example/michael/myapplication/OutfitActivity.java
+++ app/src/main/java/com/example/michael/myapplication/OutfitActivity.java
@@ -25,7 +25,7 @@ import java.util.List;
 
 public class OutfitActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    List<Entry> outfitList; //switch to wearable later for clothing and outfits
+    List<Outfit> outfitList; //switch to wearable later for clothing and outfits
 
     private final static int ROWS_WIDE = 3;
 
